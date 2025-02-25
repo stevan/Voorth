0b0318b9ca2dc3c2080910e51f0307ea3af91d08
Stevan Little <stevan.little@gmail.com>
2025-02-25 16:03:11 +0100

    stuff

--- src/Voorth/Tether.ts
+++ src/Voorth/Tether.ts
@@ -24,39 +24,32 @@ export class Tether {
     *stream () : VM.InstructionStream {
         let library = this.runtime.library;
         while (this.tapes.length) {
-            let tape = this.tapes.shift() as Tapes.CompiledTape;
+            let compiled = this.tapes.shift() as Tapes.CompiledTape;
+            let tape     = new Tapes.ExecutableTape(compiled, this.runtime);
             for (const t of tape.play()) {
                 switch (true) {
-                case CompiledTokens.isConstToken(t):
+                case ExecTokens.isConstToken(t):
                     yield { type : 'CONST', value : t.literal.toNative() as VM.Literal };
                     break;
-                case CompiledTokens.isCallToken(t):
-                    let ref = t.wordRef as Literals.WordRef;
-                    let word;
-                    if (word = library.lookup(ref)) {
-                        if (Words.isNativeWord(word)) {
-                            yield { type : 'OP', value : ref.name as VM.BIF };
-                        }
-                        else {
-                            yield* (new Tether(this.runtime, word.body)).stream();
-                        }
-                    }
-                    else {
-                        throw new Error(`Could not find word(${ref.name})`);
-                    }
+                case ExecTokens.isInvokeToken(t):
+                    throw new Error("TODO");
                     break;
-                case CompiledTokens.isMoveToken(t):
-                    let jump = t.jump;
-                    if (jump.conditional) {
-                        let cond = yield { type : 'OP', value : 'JUMP?' };
-                        if (!cond) tape.jump(jump.offset);
-                        // send NOOP instruction that can be ignored
-                        yield { type : 'OP', value : 'NOOP' };
-                    }
-                    else {
-                        tape.jump(jump.offset);
-                        continue;
-                    }
+                case ExecTokens.isCallToken(t):
+                    let userWord = t.word as Words.UserWord;
+                    yield* (new Tether(this.runtime, userWord.body)).stream();
+                    break;
+                case ExecTokens.isBuiltinToken(t):
+                    let builtinWord = t.word as Words.NativeWord;
+                    yield { type : 'OP', value : builtinWord.name as VM.BIF };
+                    break;
+                case ExecTokens.isCondToken(t):
+                    let cond = yield { type : 'OP', value : 'JUMP?' };
+                    if (!cond) tape.jump(t.offset);
+                    // send NOOP instruction that can be ignored
+                    yield { type : 'OP', value : 'NOOP' };
+                    break;
+                case ExecTokens.isMoveToken(t):
+                    tape.jump(t.offset);
                     break;
                 default:
                     throw new Error(`Unrecognized token ${JSON.stringify(t)}`)
@@ -64,5 +57,4 @@ export class Tether {
             }
         }
     }
-
 }
