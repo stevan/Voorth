c4942566e27d5d1f81f67aeb2d182a872fd5c9a0
Stevan Little <stevan.little@gmail.com>
2025-02-25 21:28:45 +0100

    cleanin

--- src/Voorth/VM.ts
+++ src/Voorth/VM.ts
@@ -44,149 +44,155 @@ export namespace VM {
         }
 
         run () : void {
-            let stack   = this.stack;
-            let control = this.control;
-            let stream  = this.tether.stream();
+            let stream = this.tether.stream();
             for (const inst of stream) {
-                console.log("Running ...", inst);
-                switch (true) {
-                case isConstant(inst):
-                    stack.push(inst.value);
+                this.execute(inst, stream, this.stack, this.control);
+            }
+            console.log("  STACK: ", this.stack);
+            console.log("CONTROL: ", this.control);
+        }
+
+        execute (
+            inst    : Instruction,
+            stream  : InstructionStream,
+            stack   : Stack,
+            control : Control,
+        ) : void {
+            console.log("EXECUTE:", inst);
+            switch (true) {
+            case isConstant(inst):
+                stack.push(inst.value);
+                break;
+            case isOperator(inst):
+                let rhs, lhs, x, y, z;
+                switch (inst.value) {
+                case 'JUMP?':
+                    x = stack.pop() as Literal;
+                    stream.next(x);
                     break;
-                case isOperator(inst):
-                    let rhs, lhs, x, y, z;
-                    switch (inst.value) {
-                    case 'JUMP?':
-                        x = stack.pop() as Literal;
-                        stream.next(x);
-                        break;
-                    case 'CALL?':
-                        x = stack.pop() as Literal;
-                        stream.next(x);
-                        break;
-                    // stack
-                    case 'DROP':
-                        stack.pop();
-                        break;
-                    case 'DUP':
-                        stack.push(stack.at(-1) as Literal);
-                        break;
-                    case 'OVER':
-                        stack.push(stack.at(-2) as Literal);
-                        break;
-                    case 'RDUP':
-                        stack.push(stack.at(-3) as Literal);
-                        break;
-                    case 'SWAP':
-                        x = stack.pop() as Literal;
-                        y = stack.pop() as Literal;
-                        stack.push(x);
-                        stack.push(y);
-                        break;
-                    case 'ROT':
-                        x = stack.pop() as Literal;
-                        y = stack.pop() as Literal;
-                        z = stack.pop() as Literal;
-                        stack.push(y);
-                        stack.push(x);
-                        stack.push(z);
-                        break;
-                    case '-ROT':
-                        x = stack.pop() as Literal;
-                        y = stack.pop() as Literal;
-                        z = stack.pop() as Literal;
-                        stack.push(x);
-                        stack.push(z);
-                        stack.push(y);
-                        break;
-                    // control
-                    case '>R!':
-                        control.push(stack.pop() as Literal);
-                        break;
-                    case '<R!':
-                        stack.push(control.pop() as Literal);
-                        break;
-                    case '.R!':
-                        stack.push(control.at(-1) as Literal);
-                        break;
-                    case '^R!':
-                        control.pop();
-                        break;
-                    // string
-                    case '~':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs.toString() + rhs.toString());
-                        break;
-                    // math
-                    case '+':
-                        rhs = stack.pop() as number;
-                        lhs = stack.pop() as number;
-                        stack.push(lhs + rhs);
-                        break;
-                    case '-':
-                        rhs = stack.pop() as number;
-                        lhs = stack.pop() as number;
-                        stack.push(lhs - rhs);
-                        break;
-                    case '*':
-                        rhs = stack.pop() as number;
-                        lhs = stack.pop() as number;
-                        stack.push(lhs * rhs);
-                        break;
-                    case '/':
-                        rhs = stack.pop() as number;
-                        lhs = stack.pop() as number;
-                        stack.push(lhs / rhs);
-                        break;
-                    case '%':
-                        rhs = stack.pop() as number;
-                        lhs = stack.pop() as number;
-                        stack.push(lhs % rhs);
-                        break;
-                    // equality
-                    case '==':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs == rhs);
-                        break;
-                    case '!=':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs != rhs);
-                        break;
-                    // comparisons
-                    case '<':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs < rhs);
-                        break;
-                    case '>':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs > rhs);
-                        break;
-                    case '>=':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs >= rhs);
-                        break;
-                    case '<=':
-                        rhs = stack.pop() as Literal;
-                        lhs = stack.pop() as Literal;
-                        stack.push(lhs <= rhs);
-                        break;
-                    default:
-                        throw new Error(`Unrecognized operator ${inst}`);
-                    }
+                case 'CALL?':
+                    x = stack.pop() as Literal;
+                    stream.next(x);
+                    break;
+                // stack
+                case 'DROP':
+                    stack.pop();
+                    break;
+                case 'DUP':
+                    stack.push(stack.at(-1) as Literal);
+                    break;
+                case 'OVER':
+                    stack.push(stack.at(-2) as Literal);
+                    break;
+                case 'RDUP':
+                    stack.push(stack.at(-3) as Literal);
+                    break;
+                case 'SWAP':
+                    x = stack.pop() as Literal;
+                    y = stack.pop() as Literal;
+                    stack.push(x);
+                    stack.push(y);
+                    break;
+                case 'ROT':
+                    x = stack.pop() as Literal;
+                    y = stack.pop() as Literal;
+                    z = stack.pop() as Literal;
+                    stack.push(y);
+                    stack.push(x);
+                    stack.push(z);
+                    break;
+                case '-ROT':
+                    x = stack.pop() as Literal;
+                    y = stack.pop() as Literal;
+                    z = stack.pop() as Literal;
+                    stack.push(x);
+                    stack.push(z);
+                    stack.push(y);
+                    break;
+                // control
+                case '>R!':
+                    control.push(stack.pop() as Literal);
+                    break;
+                case '<R!':
+                    stack.push(control.pop() as Literal);
+                    break;
+                case '.R!':
+                    stack.push(control.at(-1) as Literal);
+                    break;
+                case '^R!':
+                    control.pop();
+                    break;
+                // string
+                case '~':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs.toString() + rhs.toString());
+                    break;
+                // math
+                case '+':
+                    rhs = stack.pop() as number;
+                    lhs = stack.pop() as number;
+                    stack.push(lhs + rhs);
+                    break;
+                case '-':
+                    rhs = stack.pop() as number;
+                    lhs = stack.pop() as number;
+                    stack.push(lhs - rhs);
+                    break;
+                case '*':
+                    rhs = stack.pop() as number;
+                    lhs = stack.pop() as number;
+                    stack.push(lhs * rhs);
+                    break;
+                case '/':
+                    rhs = stack.pop() as number;
+                    lhs = stack.pop() as number;
+                    stack.push(lhs / rhs);
+                    break;
+                case '%':
+                    rhs = stack.pop() as number;
+                    lhs = stack.pop() as number;
+                    stack.push(lhs % rhs);
+                    break;
+                // equality
+                case '==':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs == rhs);
+                    break;
+                case '!=':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs != rhs);
+                    break;
+                // comparisons
+                case '<':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs < rhs);
+                    break;
+                case '>':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs > rhs);
+                    break;
+                case '>=':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs >= rhs);
+                    break;
+                case '<=':
+                    rhs = stack.pop() as Literal;
+                    lhs = stack.pop() as Literal;
+                    stack.push(lhs <= rhs);
                     break;
                 default:
-                    throw new Error(`Unrecognized instruction ${inst}`);
+                    throw new Error(`Unrecognized operator ${inst}`);
                 }
-                console.log("  STACK: ", stack);
-                console.log("CONTROL: ", control);
+                break;
+            default:
+                throw new Error(`Unrecognized instruction ${inst}`);
             }
-
             console.log("  STACK: ", stack);
             console.log("CONTROL: ", control);
         }
